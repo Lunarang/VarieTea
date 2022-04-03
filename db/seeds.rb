976bd161f3428827cc52a6058077a9b240d18ad9
Lunarang <ecmettille@gmail.com>
2022-04-03 16:59:36 -0700

    seeds established: 1 user, 5 teas, 5 benefits, 10 ingredients, and their associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Erin", email: "erinsmells@gmail.com", password: "password")
+
+teas = [
+   {name: "Green", preparation: "Steep 3 minutes"}, 
+    {name: "Lemon & Echinacea", preparation: "Steep 4 minutes"}, 
+    {name: "Chamomile & Lavender", preparation: "Steep 4 minutes"}, 
+    {name: "Orange Spice Black", preparation: "Steep 3-5 minutes"}, 
+    {name: "Pomegranate, Blueberry, & Acai Green", preparation: "Steep 1-2 minutes"}
+]
+teas.each do |t|
+    Tea.create! t
+end
+
+benefits = [
+    {name: "Energizing"}, 
+    {name: "Calming"}, 
+    {name: "Immune Support"}, 
+    {name: "Digestive Support"}, 
+    {name: "Anti-Inflammatory"}
+]
+benefits.each do |b|
+    Benefit.create! b
+end
+
+ingredients = [
+    {name: "Lemon"}, 
+    {name: "Echinacea"}, 
+    {name: "Chamomile"}, 
+    {name: "Lavender"}, 
+    {name: "Pomegranate"}, 
+    {name: "Blueberry"}, 
+    {name: "Acai"}, 
+    {name: "Orange Peel"}, 
+    {name: "Clove"}, 
+    {name: "Cinnamon"}
+]
+ingredients.each do |i|
+    Ingredient.create! i
+end
+
+Tea.find(1).benefits << [Benefit.find(1), Benefit.find(5)]
+
+Tea.find(2).benefits << Benefit.find(3)
+Tea.find(2).ingredients << [Ingredient.find(1), Ingredient.find(2)]
+
+Tea.find(3).benefits << [Benefit.find(2), Benefit.find(5)]
+Tea.find(3).ingredients << [Ingredient.find(3), Ingredient.find(4)]
+
+Tea.find(4).benefits << Benefit.find(1)
+Tea.find(4).ingredients << [Ingredient.find(8), Ingredient.find(9), Ingredient.find(10)]
+
+Tea.find(5).benefits << [Benefit.find(1), Benefit.find(5)]
+Tea.find(5).ingredients << [Ingredient.find(5), Ingredient.find(6), Ingredient.find(7)]
