baafd11532d2fee207bc2550b5a7051e44592c4c
Lunarang <ecmettille@gmail.com>
2022-04-03 17:57:31 -0700

    established basic routes for all resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static#home'
 
+  # Third party authorization
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  # static_controller
+  get 'badges', to: 'static#badges'
+
+  # user scope method urls
+  get '/users/:id/favorites', to: 'users#favorites'
+  get '/users/:id/tasted', to: 'users#tasted'
+
+  resources :teas, only: [:index, :show] #view all teas, or single tea profile
+  resources :benefits
+  resources :ingredients
+  
 end
