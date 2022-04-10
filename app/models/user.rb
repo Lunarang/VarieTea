1b5d303674fd295663bb839e29a4927943e0d39b
Lunarang <ecmettille@gmail.com>
2022-04-09 21:21:25 -0700

    added 'favorite' and 'unfavorite' public methods to add to user's new 'favs' attribute for easier access than searching through join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,54 @@ class User < ApplicationRecord
 
   after_create :add_badges
 
+  attr_accessor :favs
+
+  # Public methods
+
+  def favorite!(tea)
+    tea_saved = UserTea.find_by(user_id: self.id, tea_id: tea.id)
+    if tea_saved
+        tea_saved.favorite = true
+        tea_saved.save!
+    else
+        self.teas << tea
+        tea_saved.favorite = true
+        tea_saved.save!
+    end
+  end
+
+  def unfavorite!(tea)
+      tea_saved = UserTea.find_by(user_id: self.id, tea_id: tea.id)
+      if tea_saved
+          tea_saved.favorite = false
+          tea_saved.save!
+      end
+  end
+
   #It is considered good practice to declare callback methods as private. 
   #If left public, they can be called from outside of the model and violate the principle of object encapsulation.
   private
+
     def add_badges
       badges = Badge.create([
-          {user_id: "#{self.id}", title: "Tiny Taster", body: "You've tasted at least 1 tea!"}, 
-          {user_id: "#{self.id}", title: "Taste Tester", body: "You've tasted at least 5 teas!"}, 
-          {user_id: "#{self.id}", title: "I Came, I Saw, I Tasted", body: "You've tasted at least 10 teas!"}, 
-          {user_id: "#{self.id}", title: "Flavor Fav", body: "You've added at least 1 tea to your favorites!"}, 
-          {user_id: "#{self.id}", title: "Super Flavor Fav", body: "You've added at least 5 teas to your favorites!"}
-          ])
+        {user_id: "#{self.id}", title: "Tiny Taster", body: "You've tasted at least 1 tea!"}, 
+        {user_id: "#{self.id}", title: "Taste Tester", body: "You've tasted at least 5 teas!"}, 
+        {user_id: "#{self.id}", title: "I Came, I Saw, I Tasted", body: "You've tasted at least 10 teas!"}, 
+        {user_id: "#{self.id}", title: "Flavor Fav", body: "You've added at least 1 tea to your favorites!"}, 
+        {user_id: "#{self.id}", title: "Super Flavor Fav", body: "You've added at least 5 teas to your favorites!"}
+        ])
       self.badges.push(badges)
     end
 
-    # def favorites here
+    def favorites
+      favs_list = []
+      self.teas.each do |tea|
+        if tea.favorite?
+          favs_list << tea
+        end
+        @favs = favs_list
+      end
+    end
 
     #def tasted here
 
