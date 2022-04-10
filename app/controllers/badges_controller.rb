0b541f5a956029bf566405b030ec2ebca47b90bb
Lunarang <ecmettille@gmail.com>
2022-04-09 21:19:18 -0700

    added authentication to badge index specifically as only the user should see a badge index that reflects their own badges

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -3,9 +3,9 @@ class BadgesController < ApplicationController
     def index
         if user_signed_in?
             @user = current_user
-            @badges = @user.badges.all
+            @badges = @user.badges
        else
-           redirect badges_path
+           redirect root_path
        end
     end
 
