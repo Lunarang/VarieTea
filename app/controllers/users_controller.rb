c6509a21c702e077f879cfc8b55d697543a87e5b
Lunarang <ecmettille@gmail.com>
2022-04-11 21:15:32 -0700

    added user favorites page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,19 @@ class UsersController < ApplicationController
     def show
         @user = current_user
         @total_favs = @user.total_favorites
+        @favorites = @user.all_favorites
     end
     
     #GET - Read/Show => /users/:id/teas
     def teas
         @user = current_user
+        @teas = @user.all_teas
+    end
+
+    #GET - Read/Show => /users/:id/favorites
+    def favorites
+        @user = current_user
+        @favorites = @user.all_favorites
     end
 
 end
