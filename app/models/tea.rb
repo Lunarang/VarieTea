c8324d0481aa9c2916b332643302257a00a16e26
Lunarang <ecmettille@gmail.com>
2022-04-16 16:05:12 -0700

    tea model now accepts nested attributes

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -7,7 +7,27 @@ class Tea < ApplicationRecord
     has_many :benefits, through: :tea_benefits
 
     validates :name, presence: true, uniqueness: { case_sensitive: false }
-    validates :preparation, presence: true
+    validates_presence_of :preparation
+
+    accepts_nested_attributes_for :benefits, :ingredients
 
     scope :newest, -> { order(created_at: :desc).limit(3) }
+
+    def benefits_attributes=(benefit_attributes)
+      benefit_attributes.values.each do |benefit_attribute|
+        if benefit_attribute["name"].present?
+          benefit = Benefit.find_or_create_by(benefit_attribute)
+          self.benefits << benefit
+        end
+      end
+    end
+
+    def ingredients_attributes=(ingredient_attributes)
+      ingredient_attributes.values.each do |ingredient_attribute|
+        if ingredient_attribute["name"].present?
+          ingredient = Ingredient.find_or_create_by(ingredient_attribute)
+          self.ingredients << ingredient
+        end
+      end
+    end
 end
