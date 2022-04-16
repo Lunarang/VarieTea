b754a48db1ea8fc04445c1e782555de899d7e24e
Lunarang <ecmettille@gmail.com>
2022-04-16 16:04:07 -0700

    added button styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
     def toggle_favorite(tea)
         tea_saved = UserTea.find_by(user_id: current_user.id, tea_id: tea.id)
         if tea_saved && tea_saved.favorite == true
-          button_to "Unfavorite", favorite_path(tea), method: :delete
+          button_to "Unfavorite", favorite_path(tea), method: :delete, class:"btn btn-outline-danger"
         else
-          button_to "Favorite", favorites_path(id: tea.id), method: :post
+          button_to "Favorite", favorites_path(id: tea.id), method: :post, class:"btn btn-outline-danger"
         end
     end
 
