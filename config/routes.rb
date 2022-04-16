53519379765dff83cf449df92ff16cf29493b8a5
Lunarang <ecmettille@gmail.com>
2022-04-16 16:25:45 -0700

    fixed tea routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     get 'favorites', to: 'users#favorites'
     get 'teas', to: 'users#teas'
     resources :badges, only: [:index] #view index only for user
-    resources :teas, only: [:new, :edit, :update] #new/create & edit/update options for user submitted data
+    resources :teas, only: [:new, :edit] #new/create & edit/update options for user submitted data
     resources :benefits, only: [:new, :create, :edit, :update]
     resources :ingredients, only: [:new, :create, :edit, :update]
   end
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   # tea scope method url
   get '/teas/newest', to: 'teas#newest'
  
-  resources :teas, only: [:index, :show, :create] #view all teas, or single tea profile
+  resources :teas, only: [:index, :show, :create, :update, :destroy] #view all teas, or single tea profile
   resources :benefits, only: [:index, :show]
   resources :ingredients, only: [:index, :show]
 
