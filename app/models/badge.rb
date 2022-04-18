e0bba7b4b034c446d88bb634f68be4711f9ff041
Lunarang <ecmettille@gmail.com>
2022-04-17 18:44:54 -0700

    added scope methods

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,7 +1,6 @@
 class Badge < ApplicationRecord
     belongs_to :user
 
-    validates :title, uniqueness: { case_sensitive: false }
-
-    scope :earned, -> { where("earned = ?", true) }
+    scope :earned, ->(user_id) { where(earned: true, user_id: user_id) }
+    scope :not_earned, ->(user_id) { where(earned: false, user_id: user_id) }
 end
